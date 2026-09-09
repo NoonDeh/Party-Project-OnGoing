9ba01d6d8cc19261918bb4fcc050a2f37092c8c6
NoonD <rioivanders2@gmail.com>
2026-09-09 15:12:45 +0700

    Memperbarui Tampilan (Progress)

--- app/src/main/java/com/example/apk/MainActivity.java
+++ app/src/main/java/com/example/apk/MainActivity.java
@@ -2,15 +2,12 @@ package com.example.apk;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import java.text.NumberFormat;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
